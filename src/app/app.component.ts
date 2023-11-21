c8c9252f47bcd092f8df7aca479907979920db20
vitun bajwa <vitunbajwawins@gmail.com>
2023-11-21 11:39:35 +0530

    recaptcha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+// import { ReCaptcha2Component } from 'ngx-captcha';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recaptcha';
+  contactForm!: FormGroup;
+  public captchaResolved : boolean = false;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      name: [null, Validators.required],
+      title: [null, Validators.required],
+      phone: [null],
+      message: [null, Validators.required],
+      recaptcha: [null, Validators.required],
+    });
+  }
+
+  resolved(captchaResponse: any) {
+    this.captchaResolved = (captchaResponse && captchaResponse.length > 0) ? true : false
+    console.log(`Resolved captcha with response: ${captchaResponse}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RecaptchaModule, RecaptchaFormsModule } from 'ng-recaptcha';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
